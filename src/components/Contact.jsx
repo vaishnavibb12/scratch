22e012e6ae855feac51d29305e5cdd2170d29e89
vaishnavibb12 <vaishabiramane@gmail.com>
2024-06-06 18:45:08 +0530

    done form validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,9 +6,71 @@ import Addressdemo from "./Addressdemo";
 import { Routes, Route, Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import React, { useState } from "react";
 
 function Contact() {
   const notify = () => toast("Message Send Sucessfully😁👍!");
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = validateForm(formData);
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+      // Form submission logic here
+      // notify();
+      toast.success("Message Send Sucessfully!!😁👍❤");
+      console.log("Form submitted successfully!");
+    } else {
+      toast.error("Enter Details");
+      console.log(`Form submission failed
+         due to validation errors.`);
+    }
+  };
+
+  const validateForm = (data) => {
+    const errors = {};
+
+    if (!data.username.trim()) {
+      errors.username = "Username is required";
+    }
+
+    if (!data.email.trim()) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+      errors.email = "Email is invalid";
+    }
+
+    if (!data.password) {
+      errors.password = "Password is required";
+    } else if (data.password.length < 8) {
+      errors.password = `Password must be at 
+        least 8 characters long`;
+    }
+
+    // if (data.confirmPassword !== data.password) {
+    //   errors.confirmPassword = "Passwords do not match";
+    // }
+
+    return errors;
+  };
+
   return (
     <div className="section">
       <div className="container">
@@ -39,33 +101,49 @@ function Contact() {
 
           <div className="col-lg-4">
             <div className="input-form">
-              <form action="#">
+              <form onSubmit={handleSubmit}>
                 <div className="col-lg-12">
                   <fieldset>
                     <input
-                      // ref="name"
+                      className="form-input"
                       type="text"
-                      size="30"
-                      placeholder="Name"
+                      placeholder="User Name"
+                      name="username"
+                      value={formData.username}
+                      onChange={handleChange}
                     />
+                    {errors.username && (
+                      <span className="error-message">{errors.username}</span>
+                    )}
                     <br />
                     <input
-                      // refs="email"
-                      type="text"
-                      size="30"
+                      className="form-input"
                       placeholder="Email"
+                      type="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
                     />
+                    {errors.email && (
+                      <span className="error-message">{errors.email}</span>
+                    )}
                     <br />
                     <input
-                      // refs="phone"
-                      type="text"
-                      size="30"
-                      placeholder="Phone"
+                      className="form-input"
+                      type="password"
+                      placeholder="Password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
                     />
+                    {errors.password && (
+                      <span className="error-message">{errors.password}</span>
+                    )}
+
                     <br />
                   </fieldset>
                 </div>
-                <div className="col-lg-12">
+                {/* <div className="col-lg-12">
                   <fieldset>
                     <textarea
                       // refs="message"
@@ -75,14 +153,10 @@ function Contact() {
                       placeholder="Message"
                     />
                   </fieldset>
-                </div>
+                </div> */}
                 <div className="col-md-12">
                   <fieldset>
-                    <button
-                      onClick={notify}
-                      type="button"
-                      className="btn btn-primary"
-                    >
+                    <button type="submit" className="btn btn-primary">
                       Submit
                     </button>
                     <ToastContainer />
